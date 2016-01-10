9b51d3e078b948b89aa4acf08b7dc0b812cdcbf8
Johan Mattsson <johan.mattsson.m@gmail.com>
2016-01-10 17:40:45 +0100

    Fix pkg config version

--- scripts/pkgconfig.py
+++ scripts/pkgconfig.py
@@ -1,4 +1,5 @@
 from scripts import config
+from scripts import version
 
 def generate_pkg_config_file():
         f = open('./build/xmlbird.pc', 'w+')
@@ -9,7 +10,7 @@ libdir=${exec_prefix}/lib
 
 Name: xmlbird
 Description: XML parser
-Version: 1.0.0
+Version: """ + version.XMLBIRD_VERSION + """
 Cflags: -I${includedir}
 Libs: -L${libdir} -lxmlbird
 """)
